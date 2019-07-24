ce74bea4192e740ae580a94e50f994a1b040b7ab
jdalamo <alamojad@gmail.com>
2019-07-23 19:44:54 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+const http = require('http');
+const bodyParser = require('body-parser');
+const mySql = require('mysql');
+
+const app = express();
+
+const defaultAlbum = 'spain_summer_2019';
+
+app.use(bodyParser.urlencoded({
+    extended: false // in future, should set to true if I want to parse nested POSTs
+}));
+
+const dbConn = mySql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'website_db'
+});
+dbConn.connect((err) => {
+    if (err) throw err;
+    console.log('connected to mysql db');
+});
+
+// load view engine
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: path.join(__dirname, '/views/layouts')}));
+app.set('views', path.join(__dirname, 'views/pages'));
+app.set('view engine', 'hbs');
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'static')));
+
+// route for home page
+app.get('/', function(req, res) {
+    res.render('homeView', {title: 'JD del Alamo'});
+});
+
+// route for media
+app.get('/media', function(req, res) {
+    var album = req.query.album;
+    if (album === undefined) {
+        album = defaultAlbum;
+    } else {
+        album = req.query.album;
+    }
+    getAlbumLinks(album, function(err, videoLinks) { //Because of scope, must pass a callback function into getAlbum to get result value
+        if (err) {
+            console.log('error occurred');  
+        } else {
+            console.log(videoLinks);
+            res.render('mediaView', {
+                title: 'Media',
+                videos: videoLinks
+            });
+        }
+    });
+});
+
+// post route for getting video album
+app.post('/get-album', function(req, res) {
+    console.log('got a post request');
+    videoAlbum = req.body.album_selector;
+
+    // tracking how many views each album has
+    var updateStmt = "UPDATE Albums SET views = views + 1 WHERE album_name = " + "'" + videoAlbum + "'";
+    dbConn.query(updateStmt, function (err, result, fields) {
+        if (err) {
+            console.log('error updating view count');
+        }
+    });
+    res.redirect('/media?album=' + videoAlbum);
+});
+
+// route for projects
+app.get('/projects', function(req, res) {
+    res.render('projectsView', {title: 'Projects'});
+});
+
+// route for blog
+app.get('/blog', function(req, res) {
+    res.render('blogView', {title: 'Blog'});
+});
+
+// route for contact
+app.get('/contact', function(req, res) {
+    res.render('contactView', {title: 'Contact'});
+});
+
+app.listen(3000, function() {
+    console.log("listening on port 3000");
+});
+
+function getAlbumLinks(videoAlbum, callback) { //Because of scope, must pass a callback function into getAlbum to get result value
+    var selectStmt = "SELECT link, favorite FROM album_view WHERE album_name = " + "'" + videoAlbum + "'";
+    dbConn.query(selectStmt, function (err, result, fields) {
+        if (err) 
+            callback(err, null);
+        else
+            callback(null, result);
+    });
+}
---
+++ website_db_ddl.sql
@@ -0,0 +1,131 @@
+-- Tables
+DROP TABLE Videos;
+DROP TABLE Albums;
+
+CREATE TABLE Albums (
+    album_id     INTEGER         NOT NULL PRIMARY KEY,
+    album_name   VARCHAR(30)     NOT NULL,
+    views        INTEGER         NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Videos (
+    vid_id       INTEGER         NOT NULL PRIMARY KEY,
+    vid_name     VARCHAR(60)     NOT NULL,
+    city         VARCHAR(30)     NOT NULL,
+    us_state     VARCHAR(30),
+    country      VARCHAR(30)     NOT NULL,
+    shot_date    DATE            NOT NULL,
+    link         VARCHAR(100)    NOT NULL,
+    favorite     BOOLEAN         NOT NULL,
+    album_id     INTEGER         NOT NULL,
+    CONSTRAINT   album_fk        FOREIGN KEY     (album_id)     REFERENCES     Albums     (album_id)
+);
+
+COMMIT;
+
+
+-- Views
+CREATE OR REPLACE VIEW album_view AS
+SELECT a.album_name, v.link, v.favorite
+FROM Albums a LEFT JOIN Videos v
+ON a.album_id = v.album_id;
+
+COMMIT;
+
+
+-- Album Inserts
+INSERT INTO Albums
+VALUES
+(1, 'sa_summer_2019', 0);
+
+INSERT INTO Albums
+VALUES
+(2, 'austin_summer_2019', 0);
+
+INSERT INTO Albums
+VALUES
+(3, 'spain_summer_2019', 0);
+
+COMMIT;
+
+
+-- Video Inserts
+INSERT INTO Videos
+VALUES
+(1, 'Elm Creek Skyline', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/zj0bRBR87-4?modestbranding=1&loop=1&playlist=zj0bRBR87-4', false, 1);
+
+INSERT INTO Videos
+VALUES
+(2, 'Brackenridge River Pan', 'San Antonio', 'Texas', 'US', '2019-06-30', 'https://www.youtube.com/embed/umcR9_Ahdq0?modestbranding=1&loop=1&playlist=umcR9_Ahdq0', false, 1);
+
+INSERT INTO Videos
+VALUES
+(3, 'Brackenridge Ducks', 'San Antonio', 'Texas', 'US', '2019-06-30', 'https://www.youtube.com/embed/d4cnCwjklO8?modestbranding=1&loop=1&playlist=d4cnCwjklO8', false, 1);
+
+INSERT INTO Videos
+VALUES
+(4, 'Brackenridge Bridge Through', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/Q2Fs1cd1YVo?modestbranding=1&loop=1&playlist=Q2Fs1cd1YVo', false, 1);
+
+INSERT INTO Videos
+VALUES
+(5, 'Brackenridge Bridge Side', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/yNDcHH9ZSy8?modestbranding=1&loop=1&playlist=yNDcHH9ZSy8', true, 1);
+
+INSERT INTO Videos
+VALUES
+(6, 'Witte Pan', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/FvIoX29cGbM?modestbranding=1&loop=1&playlist=FvIoX29cGbM', true, 1);
+
+INSERT INTO Videos
+VALUES
+(7, 'Flood Control Pan', 'San Antonio', 'Texas', 'US', '2019-07-04', 'https://www.youtube.com/embed/TXDAXcMSbtk?modestbranding=1&loop=1&playlist=TXDAXcMSbtk', true, 1);
+
+INSERT INTO Videos
+VALUES
+(8, 'Flood Control Descent', 'San Antonio', 'Texas', 'US', '2019-07-04', 'https://www.youtube.com/embed/0Du7EtxOWhA?modestbranding=1&loop=1&playlist=0Du7EtxOWhA', false, 1);
+
+INSERT INTO Videos
+VALUES
+(9, 'Greenbelt Creek', 'Austin', 'Texas', 'US', '2019-07-05', 'https://www.youtube.com/embed/JWtnSwaC-3M?modestbranding=1&loop=1&playlist=JWtnSwaC-3M', false, 2);
+
+INSERT INTO Videos
+VALUES
+(10, 'Greenbelt Skyline', 'Austin', 'Texas', 'US', '2019-07-05', 'https://www.youtube.com/embed/cPs1QxUADdg?modestbranding=1&loop=1&playlist=cPs1QxUADdg', true, 2);
+
+INSERT INTO Videos
+VALUES
+(11, 'Barcelona Soccer Skyline', 'Barcelona', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/Z6HCpnHMf9U?modestbranding=1&loop=1&playlist=Z6HCpnHMf9U', false, 3);
+
+INSERT INTO Videos
+VALUES
+(12, 'Barcelona Soccer Side', 'Barcelona', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/F1sc9_Ik3WU?modestbranding=1&loop=1&playlist=F1sc9_Ik3WU', false, 3);
+
+INSERT INTO Videos
+VALUES
+(13, 'Sitges Coast', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/cmgXBJih6Sk?modestbranding=1&loop=1&playlist=cmgXBJih6Sk', false, 3);
+
+INSERT INTO Videos
+VALUES
+(14, 'Sitges Facade', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/_XuB-dqz8cA?modestbranding=1&loop=1&playlist=_XuB-dqz8cA', false, 3);
+
+INSERT INTO Videos
+VALUES
+(15, 'Sitges Rocks', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/0ULDxytMKpM?modestbranding=1&loop=1&playlist=0ULDxytMKpM', true, 3);
+
+INSERT INTO Videos
+VALUES
+(16, 'Sitges Clocktower', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/8ME5SyG10Ek?modestbranding=1&loop=1&playlist=8ME5SyG10Ek', true, 3);
+
+INSERT INTO Videos
+VALUES
+(17, 'Poblenou Gym', 'Barcelona', NULL, 'Spain', '2019-07-16', 'https://www.youtube.com/embed/XixFfwIWVsI?modestbranding=1&loop=1&playlist=XixFfwIWVsI', false, 3);
+
+INSERT INTO Videos
+VALUES
+(18, 'Poblenou Reveal', 'Barcelona', NULL, 'Spain', '2019-07-16', 'https://www.youtube.com/embed/olKPpL33Yqk?modestbranding=1&loop=1&playlist=olKPpL33Yqk', true, 3);
+
+INSERT INTO Videos
+VALUES
+(19, 'Woodlawn Tower', 'San Antonio', 'Texas', 'US', '2019-07-20', 'https://www.youtube.com/embed/8IJdYXMGgWY?modestbranding=1&loop=1&playlist=8IJdYXMGgWY', false, 1);
+
+INSERT INTO Videos
+VALUES
+(20, 'Woodlawn Dam', 'San Antonio', 'Texas', 'US', '2019-07-20', 'https://www.youtube.com/embed/6VuYVQMjAQc?modestbranding=1&loop=1&playlist=6VuYVQMjAQc', false, 1);
