0f185e2573e0d2e1c72ffd8a60a5c7cc6d066668
jdalamo <alamojad@gmail.com>
2019-07-28 19:53:50 -0500

    updated video links

--- website_db_ddl.sql
+++ website_db_ddl.sql
@@ -52,82 +52,82 @@ COMMIT;
 -- Video Inserts
 INSERT INTO Videos
 VALUES
-(1, 'Elm Creek Skyline', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/zj0bRBR87-4?modestbranding=1&loop=1&playlist=zj0bRBR87-4', false, 1);
+(1, 'Elm Creek Skyline', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/zj0bRBR87-4?loop=1&playlist=zj0bRBR87-4', false, 1);
 
 INSERT INTO Videos
 VALUES
-(2, 'Brackenridge River Pan', 'San Antonio', 'Texas', 'US', '2019-06-30', 'https://www.youtube.com/embed/umcR9_Ahdq0?modestbranding=1&loop=1&playlist=umcR9_Ahdq0', false, 1);
+(2, 'Brackenridge River Pan', 'San Antonio', 'Texas', 'US', '2019-06-30', 'https://www.youtube.com/embed/umcR9_Ahdq0?loop=1&playlist=umcR9_Ahdq0', false, 1);
 
 INSERT INTO Videos
 VALUES
-(3, 'Brackenridge Ducks', 'San Antonio', 'Texas', 'US', '2019-06-30', 'https://www.youtube.com/embed/d4cnCwjklO8?modestbranding=1&loop=1&playlist=d4cnCwjklO8', false, 1);
+(3, 'Brackenridge Ducks', 'San Antonio', 'Texas', 'US', '2019-06-30', 'https://www.youtube.com/embed/d4cnCwjklO8?loop=1&playlist=d4cnCwjklO8', false, 1);
 
 INSERT INTO Videos
 VALUES
-(4, 'Brackenridge Bridge Through', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/Q2Fs1cd1YVo?modestbranding=1&loop=1&playlist=Q2Fs1cd1YVo', false, 1);
+(4, 'Brackenridge Bridge Through', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/Q2Fs1cd1YVo?loop=1&playlist=Q2Fs1cd1YVo', false, 1);
 
 INSERT INTO Videos
 VALUES
-(5, 'Brackenridge Bridge Side', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/yNDcHH9ZSy8?modestbranding=1&loop=1&playlist=yNDcHH9ZSy8', true, 1);
+(5, 'Brackenridge Bridge Side', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/yNDcHH9ZSy8?loop=1&playlist=yNDcHH9ZSy8', true, 1);
 
 INSERT INTO Videos
 VALUES
-(6, 'Witte Pan', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/FvIoX29cGbM?modestbranding=1&loop=1&playlist=FvIoX29cGbM', true, 1);
+(6, 'Witte Pan', 'San Antonio', 'Texas', 'US', '2019-06-26', 'https://www.youtube.com/embed/FvIoX29cGbM?loop=1&playlist=FvIoX29cGbM', true, 1);
 
 INSERT INTO Videos
 VALUES
-(7, 'Flood Control Pan', 'San Antonio', 'Texas', 'US', '2019-07-04', 'https://www.youtube.com/embed/TXDAXcMSbtk?modestbranding=1&loop=1&playlist=TXDAXcMSbtk', true, 1);
+(7, 'Flood Control Pan', 'San Antonio', 'Texas', 'US', '2019-07-04', 'https://www.youtube.com/embed/TXDAXcMSbtk?loop=1&playlist=TXDAXcMSbtk', true, 1);
 
 INSERT INTO Videos
 VALUES
-(8, 'Flood Control Descent', 'San Antonio', 'Texas', 'US', '2019-07-04', 'https://www.youtube.com/embed/0Du7EtxOWhA?modestbranding=1&loop=1&playlist=0Du7EtxOWhA', false, 1);
+(8, 'Flood Control Descent', 'San Antonio', 'Texas', 'US', '2019-07-04', 'https://www.youtube.com/embed/0Du7EtxOWhA?loop=1&playlist=0Du7EtxOWhA', false, 1);
 
 INSERT INTO Videos
 VALUES
-(9, 'Greenbelt Creek', 'Austin', 'Texas', 'US', '2019-07-05', 'https://www.youtube.com/embed/JWtnSwaC-3M?modestbranding=1&loop=1&playlist=JWtnSwaC-3M', false, 2);
+(9, 'Greenbelt Creek', 'Austin', 'Texas', 'US', '2019-07-05', 'https://www.youtube.com/embed/JWtnSwaC-3M?loop=1&playlist=JWtnSwaC-3M', false, 2);
 
 INSERT INTO Videos
 VALUES
-(10, 'Greenbelt Skyline', 'Austin', 'Texas', 'US', '2019-07-05', 'https://www.youtube.com/embed/D5H7FkoKuek?modestbranding=1&loop=1&playlist=D5H7FkoKuek', true, 2);
+(10, 'Greenbelt Skyline', 'Austin', 'Texas', 'US', '2019-07-05', 'https://www.youtube.com/embed/D5H7FkoKuek?loop=1&playlist=D5H7FkoKuek', true, 2);
 
 INSERT INTO Videos
 VALUES
-(11, 'Barcelona Soccer Skyline', 'Barcelona', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/Z6HCpnHMf9U?modestbranding=1&loop=1&playlist=Z6HCpnHMf9U', false, 3);
+(11, 'Barcelona Soccer Skyline', 'Barcelona', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/Z6HCpnHMf9U?loop=1&playlist=Z6HCpnHMf9U', false, 3);
 
 INSERT INTO Videos
 VALUES
-(12, 'Barcelona Soccer Side', 'Barcelona', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/F1sc9_Ik3WU?modestbranding=1&loop=1&playlist=F1sc9_Ik3WU', false, 3);
+(12, 'Barcelona Soccer Side', 'Barcelona', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/F1sc9_Ik3WU?loop=1&playlist=F1sc9_Ik3WU', false, 3);
 
 INSERT INTO Videos
 VALUES
-(13, 'Sitges Coast', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/cmgXBJih6Sk?modestbranding=1&loop=1&playlist=cmgXBJih6Sk', false, 3);
+(13, 'Sitges Coast', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/cmgXBJih6Sk?loop=1&playlist=cmgXBJih6Sk', false, 3);
 
 INSERT INTO Videos
 VALUES
-(14, 'Sitges Facade', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/_XuB-dqz8cA?modestbranding=1&loop=1&playlist=_XuB-dqz8cA', false, 3);
+(14, 'Sitges Facade', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/_XuB-dqz8cA?loop=1&playlist=_XuB-dqz8cA', false, 3);
 
 INSERT INTO Videos
 VALUES
-(15, 'Sitges Rocks', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/0ULDxytMKpM?modestbranding=1&loop=1&playlist=0ULDxytMKpM', true, 3);
+(15, 'Sitges Rocks', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/0ULDxytMKpM?loop=1&playlist=0ULDxytMKpM', true, 3);
 
 INSERT INTO Videos
 VALUES
-(16, 'Sitges Clocktower', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/8ME5SyG10Ek?modestbranding=1&loop=1&playlist=8ME5SyG10Ek', true, 3);
+(16, 'Sitges Clocktower', 'Sitges', NULL, 'Spain', '2019-07-15', 'https://www.youtube.com/embed/8ME5SyG10Ek?loop=1&playlist=8ME5SyG10Ek', true, 3);
 
 INSERT INTO Videos
 VALUES
-(17, 'Poblenou Gym', 'Barcelona', NULL, 'Spain', '2019-07-16', 'https://www.youtube.com/embed/XixFfwIWVsI?modestbranding=1&loop=1&playlist=XixFfwIWVsI', false, 3);
+(17, 'Poblenou Gym', 'Barcelona', NULL, 'Spain', '2019-07-16', 'https://www.youtube.com/embed/XixFfwIWVsI?loop=1&playlist=XixFfwIWVsI', false, 3);
 
 INSERT INTO Videos
 VALUES
-(18, 'Poblenou Reveal', 'Barcelona', NULL, 'Spain', '2019-07-16', 'https://www.youtube.com/embed/olKPpL33Yqk?modestbranding=1&loop=1&playlist=olKPpL33Yqk', true, 3);
+(18, 'Poblenou Reveal', 'Barcelona', NULL, 'Spain', '2019-07-16', 'https://www.youtube.com/embed/olKPpL33Yqk?loop=1&playlist=olKPpL33Yqk', true, 3);
 
 INSERT INTO Videos
 VALUES
-(19, 'Woodlawn Tower', 'San Antonio', 'Texas', 'US', '2019-07-20', 'https://www.youtube.com/embed/8IJdYXMGgWY?modestbranding=1&loop=1&playlist=8IJdYXMGgWY', false, 1);
+(19, 'Woodlawn Tower', 'San Antonio', 'Texas', 'US', '2019-07-20', 'https://www.youtube.com/embed/8IJdYXMGgWY?loop=1&playlist=8IJdYXMGgWY', false, 1);
 
 INSERT INTO Videos
 VALUES
-(20, 'Woodlawn Dam', 'San Antonio', 'Texas', 'US', '2019-07-20', 'https://www.youtube.com/embed/6VuYVQMjAQc?modestbranding=1&loop=1&playlist=6VuYVQMjAQc', false, 1);
+(20, 'Woodlawn Dam', 'San Antonio', 'Texas', 'US', '2019-07-20', 'https://www.youtube.com/embed/6VuYVQMjAQc?loop=1&playlist=6VuYVQMjAQc', false, 1);
 
 COMMIT;
